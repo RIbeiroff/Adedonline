ebf0a41610fbeb5b8359f2d34aca0ff381b271f2
Jadson Ribeiro <jadsdon-ribeiro@outlook.com>
2018-02-06 18:45:34 -0300

    Tela inicial

---
+++ Adedonline/app/src/main/java/com/example/jadso/adedonline/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.jadso.adedonline;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnEntrar, btnCriar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEntrar = (Button) findViewById(R.id.btnEntrar);
+        btnCriar = (Button) findViewById(R.id.btnCriar);
+
+        btnCriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SalaCreate.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ Adedonline/app/src/main/java/com/example/jadso/adedonline/SalaCreate.java
@@ -0,0 +1,25 @@
+package com.example.jadso.adedonline;
+
+/**
+ * Created by jadso on 06/02/2018.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class SalaCreate extends AppCompatActivity {
+    private ListView listViewCategorias;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_sala);
+
+
+    }
+
+}
